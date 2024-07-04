958f2ed90050255a96bad20160339e9443f68478
alicemorey <alicemmorey@gmail.com>
2024-07-04 16:39:54 +0200

    getAuthURL Function

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,8 +1,38 @@
-exports.hello = async (event) => {
+'use strict';
+
+const { google } = require("googleapis");
+const calendar = google.calendar("v3");
+const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
+const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
+const redirect_uris = [
+  "https://alicemorey.github.io/meetup/"
+];
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  redirect_uris[0]
+);
+
+module.exports.getAuthURL = async () => {
+  /**
+   *
+   * Scopes array is passed to the `scope` option. 
+   *
+   */
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
     body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully!",
+      authUrl,
     }),
   };
 };
