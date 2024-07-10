5d5d4768464e8a083d5f09e8bc7bd954a440d5f1
alicemorey <alicemmorey@gmail.com>
2024-07-10 16:05:39 +0200

    adding getCalendarEvents

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -77,3 +77,43 @@ module.exports.getAuthURL = async () => {
           };
         });
     };
+
+    module.exports.getCalendarEvents = async (event) => {
+      const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+      oAuth2Client.setCredentials({access_token});
+
+          return new Promise((resolve, reject) => {
+          calendar.events.list (
+            {
+              calendarId: CALENDAR_ID,
+              auth:oAuth2Client,
+              timeMin: new Date().toISOString(),
+              singleEvents: true,
+              orderBy:"startTime",
+            },
+          (error, response)=> {
+            if (error) {
+            return reject(error);
+          }
+          return resolve(response);
+        });
+      })
+        .then((results) => {
+          // Respond with OAuth token 
+          return {
+            statusCode: 200,
+            headers: {
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Credentials': true,
+            },
+            body: JSON.stringify({events: results.data.items}),
+          };
+        })
+        .catch((error) => {
+          // Handle error
+          return {
+            statusCode: 500,
+            body: JSON.stringify(error),
+          };
+        });
+    };
