0e7a844ce450c9042c4a959aac325183f9de9d32
alicemorey <alicemmorey@gmail.com>
2024-07-10 14:55:25 +0200

    edits to getAccessToken

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -51,29 +51,29 @@ module.exports.getAuthURL = async () => {
   module.exports.getAccessToken = async (event) => {
       const code = decodeURIComponent(`${event.pathParameters.code}`);
       return new Promise((resolve, reject) => {
-      oAuth2Client.getToken(code, (error, response) => {
-      if (error) {
-        return reject(error);
-      }
-      return resolve(response);
-    });
-  })
-    .then((results) => {
-    
-      return {
-        statusCode: 200,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true,
-        },
-        body: JSON.stringify(results),
-      };
-    })
-    .catch((error) => {
-      // Handle error
-      return {
-        statusCode: 500,
-        body: JSON.stringify(error),
-      };
-    });
-};
+        oAuth2Client.getToken(code, (error, response) => {
+          if (error) {
+            return reject(error);
+          }
+          return resolve(response);
+        });
+      })
+        .then((results) => {
+          // Respond with OAuth token 
+          return {
+            statusCode: 200,
+            headers: {
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Credentials': true,
+            },
+            body: JSON.stringify(results),
+          };
+        })
+        .catch((error) => {
+          // Handle error
+          return {
+            statusCode: 500,
+            body: JSON.stringify(error),
+          };
+        });
+    };
