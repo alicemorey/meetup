53b58917db6cf06c5506dc0ffb96463cf9aadc94
alicemorey <alicemmorey@gmail.com>
2024-07-25 17:50:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,4 +25,4 @@ serviceWorkerRegistration.unregister();
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-atatus.notify(new Error('Test Atatus Setup'));
+
