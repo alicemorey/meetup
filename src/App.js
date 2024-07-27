fa5f948b5c19786930f4dfd98c4e9da055241408
alicemorey <alicemmorey@gmail.com>
2024-07-27 21:50:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberofEvents';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
-import { InfoAlert, ErrorAlert } from './components/Alert';
-
+import { InfoAlert } from './components/Alert';
+import { ErrorAlert } from './components/Alert';
 
 
 import './App.css';
