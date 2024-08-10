745358e241d8f1c439170e53b3a3a3074744d162
alicemorey <alicemmorey@gmail.com>
2024-08-10 18:14:56 +0200

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -20,3 +20,21 @@ const MESSAGES_TO_IGNORE = [
   }
 
   jest.setTimeout(30000);
+
+  const { ResizeObserver } = window;
+
+beforeEach(() => {
+  //@ts-ignore
+  delete window.ResizeObserver;
+  window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
+});
+
+afterEach(() => {
+  window.ResizeObserver = ResizeObserver;
+  jest.restoreAllMocks();
+});
+
