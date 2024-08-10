525f9f30e4b54c339721f69646726fd889c7ecc7
alicemorey <alicemmorey@gmail.com>
2024-08-10 18:06:18 +0200

    Update CityEventsChart.js

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -14,11 +14,11 @@ const CityEventsChart = ({ allLocations, events }) => {
   useEffect(() => {
     setData(getData());
   }, [`${data}`]);
-
+  
   const getData = () => {
     const data = allLocations.map((location) => {
       const count = events.filter((event) => event.location === location).length
-      const city = location.split(', ')[0]
+      const city = location.split((/, | - /))[0]
       return { city, count };
     })
     return data;
