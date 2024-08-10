584d8f00696336bfa126fbd61cd89e2676957606
alicemorey <alicemmorey@gmail.com>
2024-08-10 16:25:24 +0200

    installing ScatterChart

---
+++ src/components/CityEventsChart.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import {
+  ScatterChart,
+  Scatter,
+  XAxis, YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer
+} from 'recharts';
+
+const CityEventsChart = ({ allLocations, events }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(getData());
+  }, [`${data}`]);
+
+  const getData = () => {
+    const data = allLocations.map((location) => {
+      const count = events.filter((event) => event.location === location).length
+      const city = location.split(', ')[0]
+      return { city, count };
+    })
+    return data;
+  };
+
+  return (
+    <ResponsiveContainer width="99%" height={400}>
+      <ScatterChart
+        margin={{
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 20,
+        }}
+      >
+        <CartesianGrid />
+        <XAxis type="number" dataKey="x" name="stature" unit="cm" />
+        <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+        <Scatter name="A school" data={data} fill="#8884d8" />
+      </ScatterChart>
+    </ResponsiveContainer>
+  );
+}
+
+export default CityEventsChart;
