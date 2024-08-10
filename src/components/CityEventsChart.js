64905a35bfec64430c4a638b32f2917f00004b3c
alicemorey <alicemmorey@gmail.com>
2024-08-10 17:53:35 +0200

    Update CityEventsChart.js

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -36,7 +36,8 @@ const CityEventsChart = ({ allLocations, events }) => {
       >
         <CartesianGrid />
         <XAxis type="category" dataKey="city" name="City" />
-        <YAxis type="number" dataKey="count" name="Number of events" />
+        <YAxis type="number" dataKey="count" name="Number of Events" />
+        
         <Tooltip cursor={{ strokeDasharray: '3 3' }} />
         <Scatter name="A school" data={data} fill="#8884d8" />
       </ScatterChart>
